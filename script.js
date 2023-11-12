8b80401ce1535ad781f278d5ab32b7583f4c9188
danyalliaqat <daniliaqat80@gmail.com>
2023-11-12 11:12:57 +0500

    variables

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log("hello");
+
+let name = "Nasir Hussain";
+let qualification = "MS";
+let university = "University of Karachi";
+let birth_place = "Karachi";
+let age = 30;
+let profession = "Software Engineer";
+let orgaisation = "Modulers";
+let weight_kg = 70;
+let height_ft = 5.8;
+let earnings_$ = 1000;
+
+let birth_year = 2023 - age;
+let weight_lbs = weight_kg * 2.2;
+let height_inch = height_ft * 12;
+let earnings_pkr = earnings_$ * 285;
+
+console.log("Name: ",name);
+console.log("Qualification: ",qualification);
+console.log("University: ",university);
+console.log("Birth Place: ",birth_place);
+console.log("Age: ",age);
+console.log("Profession: ",profession);
+console.log("Organisation: ",orgaisation);
+console.log("Weight in Kg: ",weight_kg);
+console.log("Height in ft: ",height_ft);
+console.log("Earnings in Dollars: ",earnings_$);
+console.log("Birth Year: ",birth_year);
+console.log("Weight in lbs: ",weight_lbs);
+console.log("Height in inches: ",height_inch);
+console.log("Earnings in pkr: ",earnings_pkr);
