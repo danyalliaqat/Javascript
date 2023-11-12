7cd94618179275026e36406adf3e58a607825cd1
danyalliaqat <daniliaqat80@gmail.com>
2023-11-12 13:00:04 +0500

    if else

--- script.js
+++ script.js
@@ -1,32 +1,85 @@
-console.log("hello");
+// console.log("hello");
 
-let name = "Nasir Hussain";
-let qualification = "MS";
-let university = "University of Karachi";
-let birth_place = "Karachi";
-let age = 30;
-let profession = "Software Engineer";
-let orgaisation = "Modulers";
-let weight_kg = 70;
-let height_ft = 5.8;
-let earnings_$ = 1000;
+// let name = "Nasir Hussain";
+// let qualification = "MS";
+// let university = "University of Karachi";
+// let birth_place = "Karachi";
+// let age = 30;
+// let profession = "Software Engineer";
+// let orgaisation = "Modulers";
+// let weight_kg = 70;
+// let height_ft = 5.8;
+// let earnings_$ = 1000;
 
-let birth_year = 2023 - age;
-let weight_lbs = weight_kg * 2.2;
-let height_inch = height_ft * 12;
-let earnings_pkr = earnings_$ * 285;
+// let birth_year = 2023 - age;
+// let weight_lbs = weight_kg * 2.2;
+// let height_inch = height_ft * 12;
+// let earnings_pkr = earnings_$ * 285;
 
-console.log("Name: ",name);
-console.log("Qualification: ",qualification);
-console.log("University: ",university);
-console.log("Birth Place: ",birth_place);
-console.log("Age: ",age);
-console.log("Profession: ",profession);
-console.log("Organisation: ",orgaisation);
-console.log("Weight in Kg: ",weight_kg);
-console.log("Height in ft: ",height_ft);
-console.log("Earnings in Dollars: ",earnings_$);
-console.log("Birth Year: ",birth_year);
-console.log("Weight in lbs: ",weight_lbs);
-console.log("Height in inches: ",height_inch);
-console.log("Earnings in pkr: ",earnings_pkr);
+// console.log("Name: ",name);
+// console.log("Qualification: ",qualification);
+// console.log("University: ",university);
+// console.log("Birth Place: ",birth_place);
+// console.log("Age: ",age);
+// console.log("Profession: ",profession);
+// console.log("Organisation: ",orgaisation);
+// console.log("Weight in Kg: ",weight_kg);
+// console.log("Height in ft: ",height_ft);
+// console.log("Earnings in Dollars: ",earnings_$);
+// console.log("Birth Year: ",birth_year);
+// console.log("Weight in lbs: ",weight_lbs);
+// console.log("Height in inches: ",height_inch);
+// console.log("Earnings in pkr: ",earnings_pkr);
+
+
+// // alert("welcome to javascript journey");
+// // alert(100);
+// // alert(20+100);
+
+// // let first_name = "danyal";
+// // let second_name = "liaqat";
+// // alert(first_name);
+// // alert(second_name);
+
+
+
+// // let Age =45;
+// // console.log(Age);
+
+// // {
+// //     let Age = 60;
+// //     console.log(Age);
+
+// // }
+// // console.log(Age);
+
+
+// var Age =45;
+// console.log(Age);
+
+// {
+//     var Age = 60;
+//     console.log(Age);
+
+// }
+// console.log(Age); 
+
+// let year = Number(prompt("Enter your birth year"));
+// console.log(typeof year);
+
+// alert("Your age is : "+ (2023-year));
+
+// x=100;
+// y= 1+x;
+
+// console.log(typeof y);
+// console.log(y);
+
+let number = Number(prompt("Enter a number: "));
+console.log(typeof number);
+
+if((number % 2)== 0){
+    console.log("The number is even");
+}else{
+    console.log("The number is odd");
+}
