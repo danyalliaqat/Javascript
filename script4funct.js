3bd7952e5a26b1b7ec70f428bbd1915d1750da37
danyalliaqat <daniliaqat80@gmail.com>
2023-12-03 12:55:10 +0500

    functions

--- script4funct.js
+++ script4funct.js
@@ -1,10 +1,87 @@
-function addition(a,b){
-    // a=10
-    // b=20
-    c=a+b
-    return c
-    //console.log(c)
-}
+// function addition(a,b){
+//     // a=10
+//     // b=20
+//     c=a+b
+//     return c
+//     //console.log(c)
+// }
 
-answer=addition(20,30)
-console.log(answer)
+// answer=addition(20,30)
+// console.log(answer)
+
+// function greet_user(){
+//     console.log("Welcome Mr Danyal")
+// }
+//calling function
+// greet_user();
+
+// a function with parameter=> parameterized function:
+// function greet_user(username){
+//     console.log(`Welcome Mr ${username}`)
+// }
+// greet_user('ali');
+
+
+// function elder(a,b){
+    
+//     if(a>b){
+//         console.log("a is elder")
+//     }
+//     else{
+//         if(b>a){
+//             console.log("b is elder")
+//         }
+//     else{
+//         console.log("no one is elder")
+//     }
+// }
+// }
+// elder(20,13)
+// elder(20,24)
+
+
+//Write a function that calculate BMI.
+//Ask user his weight(kg) and height(m)=> BMI= w/h**2;
+//18.4=> underweight
+//18.5-24.9=> normal
+//25.0-39.9=>overweight
+//40.0=>obese
+
+// function BMI_Calc(weight,height){
+    // weight= prompt("Enter Your Weight")
+    // height= prompt("Enter your Height")
+
+    // BMI= weight / (height**2);
+    // if (BMI>=40){
+    //     return "obese";
+    // }
+    // else if(BMI>=25 && BMI<40){
+    //     return "Overweight";
+    // }
+    // else if(BMI>=18.5 && BMI<25){
+    //     return "Normal";
+    // }
+    // else{
+    //     return "Underweight";
+    // }
+
+    // return BMI;
+    // alert(`Your BMI is ${BMI}`)
+// }
+// let weight_kg = parseFloat(prompt("Enter your Weight in kg"));
+// let height_m = (parseFloat(prompt("Enter your height in ft")))/3.3;
+// console.log(`Dear Sir You are ${BMI_Calc(weight_kg,height_m)}`);
+//BMI_Calc();
+
+
+//write a function that calculate tax over salary
+//100k > 15%   => salary*.15
+//70k >10%     => salary*.10
+//50k > 5%     =>       .05
+// 30k > 2%    =>       .02
+//else 1%      =>       .01
+//if some one pays tax more than 2k say he is good citizen
+
+function Tax_calc(){
+    
+}
